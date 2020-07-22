28c0832ffbaa24fcf160307980cf30d3710f23b9
Rafael Cardoso <rafafscardoso@gmail.com>
2020-07-22 07:59:32 -0300

    Pequenos ajustes

--- src/data/PostDatabase.ts
+++ src/data/PostDatabase.ts
@@ -37,7 +37,40 @@ export class PostDatabase extends Database {
       const result = await this.getConnection()
         .select('id', 'photo', 'description', 'date_create as createdAt', 'user_id as creatorUserId')
         .from(PostDatabase.TABLE_NAME)
+        .where({ id });
+      const data = { ...result[0], createdAt: moment(result[0].createdAt, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YY') };
+      const post = new GetPostOutputDTO(data.id, data.photo, data.description, data.createdAt, data.type, data.creatorUserId);
+      return post;
+    } catch (error) {
+      throw new InternalServerError(error.sqlMessage || error.message);
+    }
+  }
+
+  public getByUserId = async (input:GetPostInputDTO):Promise<GetPostOutputDTO> => {
+
+    try {
+      const user_id = input.getId();
+      const result = await this.getConnection()
+        .select('id', 'photo', 'description', 'date_create as createdAt', 'user_id as creatorUserId')
+        .from(PostDatabase.TABLE_NAME)
         .where({ user_id });
+      const data = { ...result[0], createdAt: moment(result[0].createdAt, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YY') };
+      const post = new GetPostOutputDTO(data.id, data.photo, data.description, data.createdAt, data.type, data.creatorUserId);
+      return post;
+    } catch (error) {
+      throw new InternalServerError(error.sqlMessage || error.message);
+    }
+  }
+
+  public getByUserIdAndType = async (input:GetPostInputDTO):Promise<GetPostOutputDTO> => {
+
+    try {
+      const user_id = input.getId();
+      const type = input.getType();
+      const result = await this.getConnection()
+        .select('id', 'photo', 'description', 'date_create as createdAt', 'user_id as creatorUserId')
+        .from(PostDatabase.TABLE_NAME)
+        .where({ user_id, type });
       const data = { ...result[0], createdAt: moment(result[0].createdAt, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YY') };
       const post = new GetPostOutputDTO(data.id, data.photo, data.description, data.createdAt, data.type, data.creatorUserId);
       return post;
@@ -48,6 +81,7 @@ export class PostDatabase extends Database {
 }
 
 export enum TYPE {
+  ANY = '',
   NORMAL = "Normal",
   EVENT = "Event"
 }
--- src/model/Post/GetPostInputDTO.ts
+++ src/model/Post/GetPostInputDTO.ts
@@ -1,11 +1,20 @@
+import { TYPE } from "../../data/PostDatabase";
+
 export class GetPostInputDTO {
   constructor (
-    private id:string
+    private id:string,
+    private type:TYPE = TYPE.ANY
   ) {}
 
   public getId = ():string => this.id;
 
+  public getType = ():string => this.type;
+
   public setId = (id:string):void => {
     this.id = id;
+  }
+
+  public setType = (type:TYPE):void => {
+    this.type = type;
   }
 }
